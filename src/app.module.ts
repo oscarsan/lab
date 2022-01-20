e19f1804084c433921333cfb455e7de90d568879
Oscar Sanhueza <oscar@hashdot.fi>
2022-01-20 17:07:40 +0200

    add prisma module and service
    Signed-off-by: Oscar Sanhueza <oscar@hashdot.fi>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MeasurementsModule } from './measurements/measurements.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [MeasurementsModule],
+  imports: [MeasurementsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
