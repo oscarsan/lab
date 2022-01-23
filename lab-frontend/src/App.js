90f4f07ae87a53baac7c165f3e8fb4cf84a226b1
Oscar Sanhueza <oscar@hashdot.fi>
2022-01-23 20:01:50 +0200

    main ui
    Signed-off-by: Oscar Sanhueza <oscar@hashdot.fi>

--- lab-frontend/src/App.js
+++ lab-frontend/src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// Import React
+import React from "react";
+  
+// Import Bootstrap
+import { Nav, Navbar, Container, Row, Col } 
+        from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+  
+// Import Custom CSS
+import "./App.css";
+  
+// Import from react-router-dom
+import { BrowserRouter as Router, Switch,
+    Route, Link } from "react-router-dom";
+  
+// Import other React Component
+import CreateMeasurement from 
+    "./Components/create-measurement.component";
+import EditMeasurement from 
+    "./Components/edit-measurement.component";
+import MeasurementList from 
+    "./Components/measurement-list.component";
+  
+// App Component
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand>
+                <Link to={"/create-measurement"} 
+                  className="nav-link">
+                  React MERN Stack App
+                </Link>
+              </Navbar.Brand>
+  
+              <Nav className="justify-content-end">
+                <Nav>
+                  <Link to={"/create-measurement"} 
+                    className="nav-link">
+                    Create Measurement
+                  </Link>
+                </Nav>
+  
+                <Nav>
+                  <Link to={"/measurement-list"} 
+                    className="nav-link">
+                    Measurement List
+                  </Link>
+                </Nav>
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
+  
+        <Container>
+          <Row>
+            <Col md={12}>
+              <div className="wrapper">
+                <Switch>
+                  <Route exact path="/" 
+                    component={CreateMeasurement} />
+                  <Route path="/create-measurement" 
+                    component={CreateMeasurement} />
+                  <Route path="/edit-measurement/:id" 
+                    component={EditMeasurement} />
+                  <Route path="/measurement-list" 
+                    component={MeasurementList} />
+                </Switch>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </Router>
   );
-}
-
+};
+  
 export default App;
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true, // passing true here tells the server to reflect the request origin in the response
   });
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
